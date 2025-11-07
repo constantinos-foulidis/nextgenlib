d8d8304302f69b5b1483beadc085f4e6962cd0a0
FOULIDIS Konstantinos <kfoulidi@netcompany.com>
2025-11-07 15:36:33 +0200

    Change anngular builder and fix module not found on real application

--- projects/next-genpg-ui/src/public-api.ts
+++ projects/next-genpg-ui/src/public-api.ts
@@ -4,3 +4,6 @@
 
 export * from './lib/next-genpg-ui';
 export * from './lib/components/ngx-button/ngx-button';
+export * from './lib/components/ngx-button/models/btnSize';
+export * from './lib/components/ngx-button/models/btnvariant';
+export * from './lib/components/ngx-button/models/btnLabel';
