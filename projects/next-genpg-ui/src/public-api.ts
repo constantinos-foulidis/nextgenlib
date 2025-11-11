2edbcd33ed61dbb898629f15e5a5ff956a7428ff
FOULIDIS Konstantinos <kfoulidi@netcompany.com>
2025-11-11 21:54:04 +0200

    DEV: Expose cardv1

--- projects/next-genpg-ui/src/public-api.ts
+++ projects/next-genpg-ui/src/public-api.ts
@@ -20,3 +20,4 @@ export * from './lib/components/menu/menu-item/menu-item';
 export * from './lib/layouts/main-layout/main-layout';
 export * from './lib/layouts/services/layout';
 export * from './lib/components/ngx-content-img/ngx-content-img';
+export * from './lib/components/ngx-cardv1/ngx-cardv1';
